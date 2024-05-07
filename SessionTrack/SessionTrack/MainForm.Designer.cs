4aa34cda81c2a7413e4a510865771554aeebeb3f
LoanHanu <loan.hanu90@gmail.com>
2024-05-07 18:29:12 +0700

    implemented base features

---
+++ SessionTrack/SessionTrack/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SessionTrack
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotView = new OxyPlot.WindowsForms.PlotView();
+            this.buttonLoadCSV = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMaxSpeed = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // plotView
+            // 
+            this.plotView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotView.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.plotView.Location = new System.Drawing.Point(0, 0);
+            this.plotView.Name = "plotView";
+            this.plotView.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView.Size = new System.Drawing.Size(1284, 701);
+            this.plotView.TabIndex = 0;
+            this.plotView.Text = "plotView1";
+            this.plotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // buttonLoadCSV
+            // 
+            this.buttonLoadCSV.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoadCSV.AutoSize = true;
+            this.buttonLoadCSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLoadCSV.Location = new System.Drawing.Point(12, 719);
+            this.buttonLoadCSV.Name = "buttonLoadCSV";
+            this.buttonLoadCSV.Size = new System.Drawing.Size(104, 30);
+            this.buttonLoadCSV.TabIndex = 1;
+            this.buttonLoadCSV.Text = "Load CSV...";
+            this.buttonLoadCSV.UseVisualStyleBackColor = true;
+            this.buttonLoadCSV.Click += new System.EventHandler(this.buttonLoadCSV_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1069, 724);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Max Speed: ";
+            // 
+            // textBoxMaxSpeed
+            // 
+            this.textBoxMaxSpeed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMaxSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMaxSpeed.Location = new System.Drawing.Point(1172, 721);
+            this.textBoxMaxSpeed.Name = "textBoxMaxSpeed";
+            this.textBoxMaxSpeed.Size = new System.Drawing.Size(100, 26);
+            this.textBoxMaxSpeed.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 761);
+            this.Controls.Add(this.textBoxMaxSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonLoadCSV);
+            this.Controls.Add(this.plotView);
+            this.Name = "MainForm";
+            this.Text = "Session Track";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView;
+        private System.Windows.Forms.Button buttonLoadCSV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMaxSpeed;
+    }
+}
+
---
+++ SessionTrack/SessionTrack/MainForm.cs
@@ -0,0 +1,180 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SessionTrack
+{
+    public partial class MainForm : Form
+    {
+        struct CartesianPoint
+        {
+            public double x;
+            public double y;
+            public double latitude;
+            public double longitude;
+            public double speed;
+        }
+
+        private List<(DateTime time, double lat, double lon)> rawData;
+        private (DateTime time, double lat, double lon) origin = (DateTime.Now, 0, 0);
+        private List<CartesianPoint> cartesianPoints;
+        private double maxSpeed = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private List<(DateTime time, double lat, double lon)> LoadData(string filePath)
+        {
+            DateTime oldTime = DateTime.Now;
+
+            var dataList = new List<(DateTime, double, double)>();
+            using (var reader = new StreamReader(filePath))
+            {
+                string line;
+                reader.ReadLine(); // Optionally skip the header
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split(',');
+                    var year = int.Parse(parts[0]);
+                    var month = int.Parse(parts[1]);
+                    var day = int.Parse(parts[2]);
+                    var ms = int.Parse(parts[3].Substring(parts[3].Length - 2, 2));
+                    var ss = int.Parse(parts[3].Substring(parts[3].Length - 4, 2));
+                    var mm = int.Parse(parts[3].Substring(parts[3].Length - 6, 2));
+                    var hh = int.Parse(parts[3].Remove(parts[3].Length - 6, 6));
+                    var time = new DateTime(year, month, day, hh, mm, ss, ms);
+                    var lat = double.Parse(parts[4], CultureInfo.InvariantCulture);
+                    var lon = double.Parse(parts[5], CultureInfo.InvariantCulture);
+                    this.origin.lat = double.Parse(parts[6], CultureInfo.InvariantCulture);
+                    this.origin.lon = double.Parse(parts[7], CultureInfo.InvariantCulture);
+
+                    if (time == oldTime) continue;
+
+                    dataList.Add((time, lat, lon));
+                    oldTime = time;
+                }
+            }
+            return dataList;
+        }
+
+        private double GetDistance(double lat1, double lon1, double lat2, double lon2)
+        {
+            var R = 6371; // Earth radius in km
+            var dLat = (lat2 - lat1) * Math.PI / 180;
+            var dLon = (lon2 - lon1) * Math.PI / 180;
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                    Math.Cos(lat1 * Math.PI / 180) * Math.Cos(lat2 * Math.PI / 180) *
+                    Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return R * c; // Distance in km
+        }
+
+        private List<CartesianPoint> ConvertToCartesianWithMetaData(List<(DateTime time, double lat, double lon)> data, (DateTime time, double lat, double lon) origin)
+        {
+            var results = new List<CartesianPoint>();
+            (DateTime time, double lat, double lon) prev = data[0];
+
+            foreach (var point in data)
+            {
+                var distance = GetDistance(prev.lat, prev.lon, point.lat, point.lon); // Your existing distance method
+                var timeDiff = (point.time - prev.time).TotalHours;
+                var speed = (timeDiff > 0) ? distance / timeDiff : 0;
+
+                var flatPoint = ConvertToXY(point.lat, point.lon, origin.lat, origin.lon);
+                CartesianPoint cartesianPoint;
+                cartesianPoint.x = flatPoint.x; cartesianPoint.y = flatPoint.y;
+                cartesianPoint.latitude = point.lat; cartesianPoint.longitude = point.lon;
+                cartesianPoint.speed = speed;
+                results.Add(cartesianPoint);
+
+                prev = (point.time, point.lat, point.lon);
+            }
+            return results;
+        }
+
+        private (double x, double y) ConvertToXY(double lat, double lon, double originLat, double originLon)
+        {
+            var dLat = (lat - originLat) * Math.PI / 180;
+            var dLon = (lon - originLon) * Math.PI / 180;
+            var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                    Math.Cos(originLat * Math.PI / 180) * Math.Cos(lat * Math.PI / 180) *
+                    Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var distance = 6371.0 * c;
+
+            var x = distance * Math.Cos(dLon);
+            var y = distance * Math.Sin(dLon);
+            return (x, y);
+        }
+
+        private List<(double x, double y)> ConvertToCartesian(List<(DateTime time, double lat, double lon)> data, (DateTime time, double lat, double lon) origin)
+        {
+            var earthRadiusKm = 6371.0; // Radius of the Earth in kilometers
+            var results = new List<(double x, double y)>();
+
+            foreach (var point in data)
+            {
+                var dLat = (point.lat - origin.lat) * (Math.PI / 180);
+                var dLon = (point.lon - origin.lon) * (Math.PI / 180);
+                var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                        Math.Cos(origin.lat * Math.PI / 180) * Math.Cos(point.lat * Math.PI / 180) *
+                        Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+                var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+                var distance = earthRadiusKm * c; // Distance in kilometers
+
+                var x = distance * Math.Cos(dLon);
+                var y = distance * Math.Sin(dLon);
+
+                results.Add((x, y));
+            }
+            return results;
+        }
+
+        private void PlotPoints(List<CartesianPoint> points)
+        {
+            var model = new OxyPlot.PlotModel { Title = "GPS Data in Cartesian Coordinates" };
+
+            var scatterSeries = new OxyPlot.Series.ScatterSeries
+            {
+                MarkerType = OxyPlot.MarkerType.Circle,
+            };
+
+            var lineSeries = new OxyPlot.Series.LineSeries();
+
+            foreach (var point in points)
+            {
+                scatterSeries.Points.Add(new OxyPlot.Series.ScatterPoint(point.x, point.y, tag: $"latitude: {point.latitude}\nlongitude: {point.longitude}\nspeed: {point.speed}"));
+                lineSeries.Points.Add(new DataPoint(point.x, point.y)); // Add points to the line series
+
+                if (this.maxSpeed < point.speed)
+                    this.maxSpeed = point.speed;
+
+            }
+
+            scatterSeries.TrackerFormatString = scatterSeries.TrackerFormatString + "\n{Tag}";
+            lineSeries.CanTrackerInterpolatePoints = false;
+
+            model.Series.Add(lineSeries); // Add the line series to the plot model
+            model.Series.Add(scatterSeries);
+            this.plotView.Model = model;
+        }
+
+        private void buttonLoadCSV_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Application.StartupPath;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.rawData = this.LoadData(openFileDialog.FileName);
+                this.cartesianPoints = ConvertToCartesianWithMetaData(this.rawData, this.origin);
+                this.PlotPoints(this.cartesianPoints);
+                this.textBoxMaxSpeed.Text = $"{this.maxSpeed}";
+            }
+        }
+    }
+}
